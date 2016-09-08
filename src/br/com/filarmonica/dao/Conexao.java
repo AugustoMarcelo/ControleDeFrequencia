eecedc6eae05399e65a8938cb1908022bfe36a68
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-09-08 20:02:44 -0300

    Classe para conexão com banco de dados

---
+++ src/br/com/filarmonica/dao/Conexao.java
@@ -0,0 +1,39 @@
+package br.com.filarmonica.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    
+    public static Conexao instance = null;
+    public static Connection conn = null;
+    
+    private Conexao() {
+        
+    }
+    
+    public static Conexao getInstance() {
+        if(instance == null) {
+            instance = new Conexao();
+        }
+        return instance;
+    }
+    
+    protected static Connection getConnection() {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/filarmonica";
+        String user = "root";
+        String password = "";
+        try {
+            if(conn == null) {
+                Class.forName(driver);
+                conn = DriverManager.getConnection(url, user, password);
+                return conn;
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            return null;
+        }
+        return conn;
+    }
+}
