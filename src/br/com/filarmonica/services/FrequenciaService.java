64bed521ae1becd339179f561e78d61765bad9b8
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-09-16 15:11:36 -0300

    Métodos para listar músicos adicionados

--- src/br/com/filarmonica/services/FrequenciaService.java
+++ src/br/com/filarmonica/services/FrequenciaService.java
@@ -2,6 +2,9 @@ package br.com.filarmonica.services;
 
 import br.com.filarmonica.dao.FrequenciaDAO;
 import br.com.filarmonica.models.Frequencia;
+import br.com.filarmonica.models.ModelToPDF;
+import br.com.filarmonica.models.Musico;
+import br.com.filarmonica.models.Tocata;
 import java.util.List;
 
 public class FrequenciaService {
@@ -22,5 +25,33 @@ public class FrequenciaService {
     
     public List<Frequencia> listFrequencias() {
         return frequenciaDao.listFrequencias();
+    }
+    
+    public List<Frequencia> listFrequencias(Tocata t) {
+        return frequenciaDao.listFrequencias(t);
+    }
+    
+    public List<Musico> listMusicosToTocata(Tocata t) {
+        return frequenciaDao.listMusicosToTocata(t);
+    }
+    
+    public boolean update(List<Frequencia> frequencias) {
+        return frequenciaDao.insert(frequencias);
+    }
+    
+    public boolean delete(List<Frequencia> frequencias) {
+        return frequenciaDao.delete(frequencias);
+    }
+    
+    public boolean delete(Frequencia frequencia) {
+        return frequenciaDao.delete(frequencia);
+    }
+    
+    public List<ModelToPDF> listToPDF(Tocata t) {
+        return frequenciaDao.listToPDF(t);
+    }
+    
+    public List<ModelToPDF> listToPDF(String dataInicio, String dataFim) {
+        return frequenciaDao.listToPDF(dataInicio, dataFim);
     }
 }
