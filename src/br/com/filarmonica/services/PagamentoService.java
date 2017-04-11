885b8a82c568a39f2de6bd3725c2d88272871b42
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2017-04-11 13:52:35 -0300

    Classe Service de Pagamentos adicionada.

---
+++ src/br/com/filarmonica/services/PagamentoService.java
@@ -0,0 +1,37 @@
+package br.com.filarmonica.services;
+
+import br.com.filarmonica.dao.PagamentoDAO;
+import br.com.filarmonica.models.Musico;
+import br.com.filarmonica.models.Pagamento;
+import java.util.List;
+
+/**
+ *
+ * @author Marcelo Augusto
+ */
+public class PagamentoService {
+    
+    private PagamentoDAO pagamentoDAO;
+    private MusicoService mService;
+    
+    public PagamentoService() {
+        pagamentoDAO = new PagamentoDAO();
+        mService = new MusicoService();
+    }
+    
+    public boolean save(Pagamento p) {
+        if (p.getId() == 0) {
+            return pagamentoDAO.insert(p);
+        } else {
+            return pagamentoDAO.update(p);
+        }        
+    }
+    
+    public Musico searchMusico(int id) {
+        return mService.search(id);
+    }
+    
+    public List<Pagamento> listPagamentos(int idMusico) {
+        return pagamentoDAO.listPagamentos(idMusico);
+    }
+}
