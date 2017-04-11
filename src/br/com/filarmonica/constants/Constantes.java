61cdecd05415c49f066207a79c88f9448a4bf237
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2017-04-11 13:58:03 -0300

    Retirada de comentários.

--- src/br/com/filarmonica/constants/Constantes.java
+++ src/br/com/filarmonica/constants/Constantes.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.filarmonica.constants;
 
-/**
- *
- * @author Marcelo Augusto
- */
 public enum Constantes {
     
     Version("Versão", "1.0.0"),
